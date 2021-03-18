9cb6442f7c8963626a40ce95359b147d257ce7c2
John Cooley <johndavidcooley@gmail.com>
2021-03-18 00:36:32 -0400

    Installed some packages. Created a new header and footer component.

---
+++ src/components/FooterMain.jsx
@@ -0,0 +1,13 @@
+// Node Modules
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterMain = () => (
+  <footer>
+    This is the main footer.
+  </footer>
+);
+
+export default styleMedia(FooterMain)`
+
+`;
---
+++ src/components/HeaderMain.jsx
@@ -0,0 +1,13 @@
+// Node Modules
+import React from 'react';
+import styled from 'styled-components';
+
+const HeaderMain = () => (
+  <header>
+    This is the main header.
+  </header>
+);
+
+export default styled(HeaderMain)`
+
+`;
